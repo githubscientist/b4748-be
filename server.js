0dedd5d11d55a20e905e02d3756e256389f45f3d
githubscientist <sathish.datascientist@gmail.com>
2023-11-25 17:38:52 +0530

    a simple server serving array of objects json

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 // import the library http
 const http = require('http');
 
+let notes = [
+    {
+        id: 1,
+        content: 'backend using node.js',
+        important: true
+    }, 
+    {
+        id: 2,
+        content: 'node.js is a open source',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'simple web server using Node.js',
+        important: true
+    }
+];
+
 // create a simple web server
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World!');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(notes));
 });
 
 
 const PORT = 3001;
 // make the server to listen for http requests
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
