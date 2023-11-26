92465d303cb927867fb665dee24c39437f8920e6
githubscientist <sathish.datascientist@gmail.com>
2023-11-26 16:27:09 +0530

    starting point for backend

--- writeFile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const content = '\nThe content to be written in the file!';
-
-fs.writeFile('./test.txt', content, { flag: 'a' }, (err) => {
-    if (err) console.log(err);
-    console.log('content written successfully!');
-});
--- writeFileSync.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const content = `Session 1: ${new Date().toISOString()}`;
-
-fs.writeFileSync('./test.txt', content, { flag: 'a' });
-console.log('content written successfully!');
