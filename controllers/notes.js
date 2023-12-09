9c6010079546bb145479c7c1f8b2e9dc76b76424
githubscientist <sathish.datascientist@gmail.com>
2023-12-09 17:08:36 +0530

    note crud endpoints completed

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,12 +1,87 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 
-// endpoint to view all the notes
+// endpoint to get all the notes
 notesRouter.get('/', (request, response) => {
     Note.find({}, {})
         .then(notes => {
             response.status(200).json(notes);
         })
+});
+
+// for handling query params
+notesRouter.get('/query', (request, response) => {
+    console.log(request.query);
+    console.log(request.query.id);
+    console.log(request.query.browser);
+});
+
+// endpoint to create a new resource/note based on the request data
+notesRouter.post('/', (request, response) => {
+    const note = new Note(request.body);
+
+    note.save()
+        .then(() => {
+            response.status(201).json({ message: 'note created successfully' });
+        });
+});
+
+// endpoint to fetch a single note/resource based on id
+notesRouter.get('/:id', (request, response) => {
+    // get the id from the params
+    let id = request.params.id;
+
+    Note.findById(id)
+        .then(note => {
+            response.status(200).json(note);
+        })
+        .catch(err => {
+            response.status(404).json({ message: 'id does not exist' });
+        });
+});
+
+// endpoint to delete a single resource based on id
+notesRouter.delete('/:id', (request, response) => {
+    const id = request.params.id;
+
+    Note.findByIdAndDelete(id)
+        .then(deleteNote => {
+            if (deleteNote) {
+                response.status(204).json({ message: 'note deleted successfully' });
+            }
+        });
+});
+
+// endpoint to replace the entire note object identified by an id
+notesRouter.put('/:id', (request, response) => {
+    const id = request.params.id;
+
+    const noteToUpdate = request.body;
+
+    Note.findByIdAndUpdate(id, noteToUpdate)
+        .then(updatedNote => {
+            if (updatedNote) {
+                response.status(200).json({ message: 'note replaced successfully ' });
+            } else {
+                response.status(404).json({ message: 'id does not exist' });
+            }
+        });
+});
+
+// endpoint to update a single property of a note object identified by an id
+notesRouter.patch('/:id', (request, response) => {
+    const id = request.params.id;
+
+    const noteToPatch = request.body;
+
+    Note.findByIdAndUpdate(id, noteToPatch)
+        .then(updatedNote => {
+            if (updatedNote) {
+                response.status(200).json({ message: 'note patched successfully ' });
+            } else {
+                response.status(404).json({ message: 'id does not exist' });
+            }
+        });
 });
 
 
